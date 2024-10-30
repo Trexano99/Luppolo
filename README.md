f17a77829c3506302f341bf99e969b09196af040
tex mex <viscontimax@hotmail.it>
2024-10-30 17:10:19 +0100

    Added grammar compiler

--- README.md
+++ README.md
@@ -2,5 +2,17 @@
 TODO: Introduzione al progetto
 
 
+## GUIDA ALL'UTILIZZO
+Prima di iniziare ad utilizzare LUPPOLO è necessario compilare la grammatica, che non viene fornita precompilata.
+### Compilazione della grammatica
+La compilazione della grammatica può essere effettuata o manualmente o automaticamente. La sua compilazione permette la generazione all'interno della cartella bin del `Lexer`, del `Parser`, del `Listener` e del `Visitor`, sui quali è basato il resto del funzionamento del progetto. 
+#### Compilazione automatica
+Per la compilazione automatica viene messa a disposizione la classe AntlrGrammarCompiler, che permette di compilare la grammatica in modo automatico.<br>
+Per avviare la compilazione basta eseguire il metodo `compileGrammar()` della classe `AntlrGrammarCompiler`. Vedere la documentazione del metodo per ulteriori informazioni sui parametri.<br>
+#### Compilazione manuale
+Per la compilazione manuale è necessario utilizzare il jar di antlr4, che può essere scaricato dal sito ufficiale di antlr4. Una volta scaricato il jar, è possibile compilare la grammatica con il comando `java -jar [YOUR ANTLR_JAR] -Dlanguage=Python3 -visitor -o bin/ grammar\syntax\luppolo.g`.
+
+
+
 ## TEST
 Per approfondire la parte di test fare riferimento al file [README_TEST.md](test/README_TEST.md)
---
+++ src/grammar/AntlrGrammarCompiler.py
@@ -0,0 +1,32 @@
+
+
+
+import subprocess
+
+from utils.LuppoloLogger import LuppoloLogger
+class AntlrGrammarCompiler:
+
+    @staticmethod
+    def compileGrammar(
+            syntaxFilePath = 'grammar\\syntax\\luppolo.g',
+            targetDir = '../bin',
+            antlrJarPath = '%ANTLR4_JAR%'):
+        '''
+        This method compiles the grammar file using the ANTLR4 tool.
+        The method takes the following parameters:
+        - syntaxFilePath: the path of the grammar file to compile. Default value is 'grammar\\syntax\\luppolo.g'.
+        - targetDir: the directory where the compiled grammar (binary) will be saved. Default value is '../bin'.
+        - antlrJarPath: the path of the ANTLR4 jar file. Default value is '%ANTLR4_JAR%'.
+        The method returns True if the grammar is compiled successfully, False otherwise.
+        '''
+        
+        command = f'java -jar "{antlrJarPath}" -Dlanguage=Python3 -visitor -o {targetDir} {syntaxFilePath}'
+        
+        LuppoloLogger.logInfo(f'Compiling grammar.')
+        try:
+            subprocess.run(command, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            LuppoloLogger.logError(f'Error compiling grammar: {e}')
+            return False
+        LuppoloLogger.logInfo(f'Grammar compiled successfully in dir: {targetDir}.')
+        return True
