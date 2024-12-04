6a94935b46c6aa1a0029b0cced9b998b26e60d8c
tex mex <viscontimax@hotmail.it>
2024-12-04 12:06:48 +0100

    Added all functionality to parser.

--- src/interpreter/interpreter.py
+++ src/interpreter/interpreter.py
@@ -1,8 +1,15 @@
 
 from src.utils.LuppoloLogger import LuppoloLogger
 from src.ast.elements.Function import Function
+from src.ast.elements.expression.BinOp import BinOp
+from src.ast.elements.condition.BinCond import BinCond
+from src.ast.elements.condition.TrueFalse import TrueFalse
+
 
 from src.ast.elements.instruction.Assignment import Assignment
+
+from src.expression.leaf import *
+from src.expression.nodes import *
 
 class LuppoloInterpreter:
 
@@ -23,7 +30,7 @@ class LuppoloInterpreter:
         # Controllo che la funzione chiamata esista
         if funcName not in self.functions:
             LuppoloLogger.logError(err:=f"Function {funcName} not found.")
-            raise LuppoloInterpException()
+            raise LuppoloInterpException(self)
         
         # Controllo che il numero di parametri passati alla funzione sia corretto
         if len(params) != len(expFuncParams :=self.functions[funcName].funcParams):
@@ -32,7 +39,7 @@ class LuppoloInterpreter:
             error += f", got {len(params)} [{params}]."
             
             LuppoloLogger.logError(error)
-            raise LuppoloInterpException()
+            raise LuppoloInterpException(self)
         
         interpFunc : Function = self.functions[funcName]
 
@@ -42,9 +49,17 @@ class LuppoloInterpreter:
             node, visited = INSTR_STACK.pop()
             match node.__class__.__name__:
 
-                ##############
-                # ISTRUZIONI #
-                ##############
+            ##########
+            # BLOCCO #
+            ##########
+
+                case "InstrBlock":
+                    INSTR_STACK.extend([(instr, False) for instr in reversed(node.children)])
+
+
+            ##############
+            # ISTRUZIONI #
+            ##############
 
                 case "Assignment":
                     # Se il nodo è già stato visitato, allora possiamo eseguire
@@ -103,7 +118,7 @@ class LuppoloInterpreter:
                     # segnalo l'errore e sollevo un'eccezione
                     if not expr.__class__.__name__ == "Rational" or expr.denominator != 1 or expr.negated:
                         LuppoloLogger.logError("Repeat expression must be a positive integer number.")
-                        raise LuppoloInterpException()
+                        raise LuppoloInterpException(self)
                     
                     INSTR_STACK.extend([(block,False) for _ in range(expr.numerator)])
 
@@ -129,13 +144,118 @@ class LuppoloInterpreter:
                     else:
                         INSTR_STACK.append(node, True)
                         INSTR_STACK.append(cond, False)
+
+
+            ###############
+            # ESPRESSIONI #
+            ###############
                 
+                case "NAT":
+                    VALUE_STACK.append(Rational(node.value), negated=node.negated)
+
+                case "SYM":
+                    VALUE_STACK.append(Symbol(node.value), negated=node.negated)
+                
+                case "ID":
+                    # Controllo che la variabile sia stata definita
+                    if node.varName not in ID_MEM:
+                        LuppoloLogger.logError(f"Variable {node.varName} not found.")
+                        raise LuppoloInterpException(self)
+                    
+                    VALUE_STACK.append(ID_MEM[node.varName].copy_with())
+
+                case "BinOp":
+                    # Se il nodo è già stato visitato, allora possiamo eseguire l'operazione
+                    if visited:
+                        left = VALUE_STACK.pop()
+                        right = VALUE_STACK.pop()
+                        match node.op:
+                            case BinOp.BinOpType.SUM:
+                                VALUE_STACK.append(Add(left, right, node.negated).simplify())
+                            case BinOp.BinOpType.SUB:
+                                right = right.copy_with(negated = not right.negated)
+                                VALUE_STACK.append(Add(left, right, node.negated).simplify())
+                            case BinOp.BinOpType.MUL:
+                                VALUE_STACK.append(Mult(left, right, node.negated).simplify())
+                            case BinOp.BinOpType.DIV:
+                                VALUE_STACK.append(Mult(left, Pow(right, Rational(-1)), node.negated).simplify())
+                            case BinOp.BinOpType.POW:
+                                VALUE_STACK.append(Pow(left, right, node.negated).simplify())
+                    # Altrimenti dobbiamo visitare i figli. Aggiungiamo allo stack prima
+                    # il nodo stesso, poi il nodo di sinistra e dunque il destro.
+                    # Così nell'esecuzione verrà eseguito prima il destro e messo nello 
+                    # stack dei valori, dunque il sinistro e infine nuovamente l'operazione.
+                    # Il primo risultato estratto sarà dunque il sinistro, il secondo il destro.
+                    else:
+                        INSTR_STACK.append(node, True)
+                        INSTR_STACK.append(node.children[0], False)
+                        INSTR_STACK.append(node.children[1], False)
 
 
+            ##############
+            # CONDIZIONI #
+            ##############                
 
+                case "BinCond":
 
+                    # Se il nodo è già stato visitato, allora possiamo eseguire la condizione
+                    if visited:
+                        left = VALUE_STACK.pop()
+                        right = VALUE_STACK.pop()
+                        match node.op:
+                            case BinCond.BinCondType.EQ:
+                                VALUE_STACK.append(left == right)
+                            case BinCond.BinCondType.GREATER:
+                                VALUE_STACK.append(right < left)
+                            case BinCond.BinCondType.GEQ:
+                                VALUE_STACK.append((right < left) or (right == left))
+                            case BinCond.BinCondType.LESS:
+                                VALUE_STACK.append(left < right)
+                            case BinCond.BinCondType.LEQ:
+                                VALUE_STACK.append((left < right) or (left == right))
+                            case BinCond.BinCondType.AND:
+                                VALUE_STACK.append(left and right)
+                            case BinCond.BinCondType.OR:
+                                VALUE_STACK.append(left or right)
 
-                   
+                    # Altrimenti dobbiamo visitare i figli. Vedi spiegazione ordine in BinOp.
+                    else:
+                        INSTR_STACK.append(node, True)
+                        INSTR_STACK.append(node.children[0], False)
+                        INSTR_STACK.append(node.children[1], False)
+
+                case "TrueFalse":
+                    VALUE_STACK.append(True if node.value == TrueFalse.TrueFalseType.TRUE else False)
+
+                
+            ##############
+            # FUNCCALL #
+            ##############  
+
+                case "FuncCall":
+                    
+                    # Se il nodo è già stato visitato, allora possiamo eseguire la funzione
+                    if visited:
+                        funcName = node.funcName
+                        params = [VALUE_STACK.pop() for _ in range(len(node.children))]
+                        
+                        VALUE_STACK.append(self.interpretFunc(funcName, params))
+
+                    # Altrimenti valutiamo i parametri e torniamo a visitare il nodo
+                    else:
+                        INSTR_STACK.append(node, True)
+                        for arg in node.args:
+                            INSTR_STACK.append(arg, False)
+                        
+
+                case _:
+                    LuppoloLogger.logError(f"Node {node.__class__.__name__} not recognized to be interpretated.")
+                    raise LuppoloInterpException(self)
+                
+        # Se non è stato restituito nulla, allora la funzione non ha un return statement
+        LuppoloLogger.logError(f"Function {funcName} has no return statement.")
+        raise LuppoloInterpException(self)
+        
 
 
 
