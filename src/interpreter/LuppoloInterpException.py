57078a0deba9dae0d0710d0c3119072619bc4d31
tex mex <viscontimax@hotmail.it>
2024-12-09 22:37:31 +0100

    Improved documentation and added control on funcMem

--- src/interpreter/LuppoloInterpException.py
+++ src/interpreter/LuppoloInterpException.py
@@ -1,18 +1,25 @@
+import sys
 
+# To remove traceback from error
+#sys.tracebacklimit=0
 
 class LuppoloInterpException(Exception):
+    '''
+    The exception class for the interpreter. It is used to raise errors during the interpretation of the program.
+    '''
 
     verboseLog = True
     defaultErrorMessage = "An error occurred during the interpretation of the program. Read logs for more info.\n"
     
-    def __init__(self,funcMem):
+    def __init__(self, funcMem=None):
+        '''
+        Initializes the exception passing the function memory at the time of the error.
+        If verboseLog is set to True, the function memory will be printed in the error message.
+        '''
         errorMessage = LuppoloInterpException.defaultErrorMessage
-        if LuppoloInterpException.verboseLog:
+        if LuppoloInterpException.verboseLog and funcMem is not None:
             errorMessage += "Function memory at the time of the error:\n"
             errorMessage += f"ID_MEM: {funcMem[0]}\n"
             errorMessage += f"INSTR_STACK: {funcMem[1]}\n"
             errorMessage += f"VALUE_STACK: {funcMem[2]}\n"
         super().__init__(errorMessage)
-
-    def __init__(self):
-        super().__init__(LuppoloInterpException.defaultErrorMessage)
